4127988fd8700452c3a67217556140a73efbef92
Siddharth <smishra1605@gmail.com>
2021-09-23 09:33:26 +0530

    milestone

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
-    { title: "Car Insurance", amount: 1234, date: new Date(2021, 8, 28) },
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 1234,
+      date: new Date(2021, 8, 28),
+    },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
